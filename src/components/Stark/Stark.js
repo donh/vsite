781d96a09fda3d2cfa76d797d12f4c78d558620e
Don <don@cepave.com>
2017-07-10 00:09:03 +0800

    add Stark component

---
+++ src/components/Stark/Stark.js
@@ -0,0 +1,94 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class Loading extends Component {
+  static propTypes = {
+    show: PropTypes.bool.isRequired
+  }
+
+  render() {
+    const styles = require('./Stark.scss');
+    const style = {
+      // display: this.props.show ? 'block' : 'none'
+      display: 'block'
+    };
+    return (
+      <div className={styles.StarkContainer} style={style}>
+        <div id={styles.logo}>
+          <div className={styles.title}>STARK INDUSTRIES</div>
+        </div>
+        <div className={styles.starkLogin}>
+          <div className={styles.loginBox}>
+            <div className={styles.starkButton}>Log In</div>
+          </div>
+          <div className={styles.hexagons}>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <br />
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+            <span>&#x2B22;</span>
+          </div>
+        </div>
+        <div id={styles.circle1}>
+          <div id={styles.innerCirlce1}></div>
+        </div>
+      </div>
+    );
+  }
+}
