e9a4a14da11492db46a9ea21c37db43442a0b06b
Don <don@cepave.com>
2017-10-20 13:21:35 +0800

    refine api.js

--- api/api.js
+++ api/api.js
@@ -6,7 +6,7 @@ import PrettyError from 'pretty-error';
 import SocketIo from 'socket.io';
 import config from '../src/config';
 import * as actions from './actions/index';
-import { mapUrl } from './utils/url.js';
+import { mapUrl } from './utils/url';
 
 const app = express();
 const pretty = new PrettyError();
